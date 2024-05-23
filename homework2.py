a7adf28aab93b74ca2de6783b144c6d412cd08a4
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-05-23 21:13:00 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+completed_homework = 12
+total_hourse = 1.5
+name_course='Python,'
+print('Курс:', name_course, ' всего задач:',completed_homework, 'затрачено часов:', total_hourse, 'среднее время выполнения:', total_hourse/completed_homework)
