9bf17d0f50fd320a06ae9d7bc9b99abee09aec3c
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-05-25 22:50:38 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = 33, 'Василий', True, ['Geometry','Algebra']
+print(immutable_var)
+mutable_list = ['Сергей', 'Анастасия', "Петр", "Елена"]
+mutable_list[1] = 'Анжелика'
+mutable_list.append("Виктор")
+mutable_list.remove("Петр")
+print(mutable_list)
