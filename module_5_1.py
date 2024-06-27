08b3828c1a8399e68f38375ce7426ffc932b0400
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-06-27 20:54:14 +0300

    Update module_5_2.py

--- module_5_1.py
+++ module_5_1.py
@@ -17,6 +17,15 @@ class House:
         self.name = name
         self.numbers_of_floors = numbers_of_floors
 
+    
+    def __len__(self):
+        return self.numbers_of_floors
+
+
+    def __str__(self):
+        return (f'Название: {self.name}, кол-во этажей: {self.numbers_of_floors}')
+
+    
     def go_to (self, new_floor):
         for i in range(1, new_floor + 1):
             if new_floor <= self.numbers_of_floors and new_floor > 0:
@@ -26,8 +35,14 @@ class House:
                 break
 
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
 
