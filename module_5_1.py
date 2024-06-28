8e3e28e53870b531bcdee1a867371440c50ac74f
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-06-28 12:55:31 +0300

    Update module_5_3.py

--- module_5_1.py
+++ module_5_1.py
@@ -17,7 +17,7 @@ class House:
         self.name = name
         self.numbers_of_floors = numbers_of_floors
 
-    
+
     def __len__(self):
         return self.numbers_of_floors
 
@@ -25,24 +25,75 @@ class House:
     def __str__(self):
         return (f'Название: {self.name}, кол-во этажей: {self.numbers_of_floors}')
 
-    
+
+    def __eq__(self, other):
+        return self.numbers_of_floors == other.numbers_of_floors
+
+
+    def __add__(self, values):
+        self.numbers_of_floors = self.numbers_of_floors + values
+        return self
+
+
+    def __iadd__(self, values):
+        self.numbers_of_floors += values
+        return self
+        
+
+    def __radd__(self, values):
+        self.numbers_of_floors = values + self.numbers_of_floors
+        return self
+
+
+    def __gt__(self, other):
+        return self.numbers_of_floors > other.numbers_of_floors
+        
+
+    def __ge__(self, other):
+        return self.numbers_of_floors >= other.numbers_of_floors
+
+
+    def __lt__(self, other):
+        return self.numbers_of_floors < other.numbers_of_floors
+
+
+    def __le__(self, other):
+        return self.numbers_of_floors <= other.numbers_of_floors
+
+
+    def __ne__(self, other):
+        return self.numbers_of_floors != other.numbers_of_floors
+
+
     def go_to (self, new_floor):
         for i in range(1, new_floor + 1):
             if new_floor <= self.numbers_of_floors and new_floor > 0:
                 print(i)
             else:
-                print('такого этажа не существует')
+                print('Такого этажа не существует')
                 break
 
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
 print(h1)
 print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
+print(h1 == h2) # __eq__
 
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
