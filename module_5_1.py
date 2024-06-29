37bbcbd3c2ede77a87be78b868580e387d7dcf21
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-06-29 20:45:09 +0300

    Update module_5_4.py

--- module_5_1.py
+++ module_5_1.py
@@ -13,9 +13,21 @@
 # Создайте объект класса House с произвольным названием и количеством этажей.
 # Вызовите метод go_to у этого объекта с произвольным числом.
 class House:
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+
     def __init__(self, name, numbers_of_floors):
         self.name = name
         self.numbers_of_floors = numbers_of_floors
+        self.houses_history.append(self.name)
+
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
 
 
     def __len__(self):
@@ -38,7 +50,7 @@ class House:
     def __iadd__(self, values):
         self.numbers_of_floors += values
         return self
-        
+
 
     def __radd__(self, values):
         self.numbers_of_floors = values + self.numbers_of_floors
@@ -47,7 +59,7 @@ class House:
 
     def __gt__(self, other):
         return self.numbers_of_floors > other.numbers_of_floors
-        
+
 
     def __ge__(self, other):
         return self.numbers_of_floors >= other.numbers_of_floors
@@ -73,27 +85,13 @@ class House:
                 print('Такого этажа не существует')
                 break
 
-
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
-
-print(h1)
-print(h2)
-
-print(h1 == h2) # __eq__
-
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+# Удаление объектов
+del h2
+del h3
+print(House.houses_history)
