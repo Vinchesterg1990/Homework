b7d61db30fffdeb73ab6e6a1092b3a5fd072b142
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-07-11 09:38:41 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,33 +1,31 @@
 class Hourse:
     def __init__(self):
-        super().__init__()
         self.x_distance = 0
         self.sound = "Frrr"
+        super().__init__()
 
     def run (self, dx):
         self.x_distance += dx
-        return self.x_distance
 
 
 class Eagle:
     def __init__(self):
-        super().__init__()
         self.y_distance = 0
         self.sound = "I train, eat, sleep, and repeat"
 
     def fly(self, dy):
         self.y_distance += dy
-        return self.y_distance
 
 class Pegasus(Hourse, Eagle):
     def __init__(self):
         super().__init__()
-        Eagle.__init__(self)
 
 
     def move(self, dx, dy):
-        return self.run(dx), self.fly(dy)
+        self.run(dx)
+        self.fly(dy)
 
+    
     def get_pos(self):
         return self.x_distance, self.y_distance
 
