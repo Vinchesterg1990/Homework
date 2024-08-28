3f95e19f8b46b7c89001e4517b535b5d93d8743b
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-08-28 20:53:47 +0300

    module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -1,24 +1,30 @@
 import multiprocessing
-import datetime
+from datetime import datetime
 
 def read_info(name):
     all_data = []
-    with open(name, encoding='utf-8') as f:
-        line = f.readline()
-        while line:
-            all_data.append(line)
-            line = f.readline()
+    file = open(name, 'r')
+    while True:
+        line = file.readline()
+        all_data.append(line)
+        if not line:
+            break
+    file.close
 
 files = ['file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt']
 
-start = datetime.datetime.now()
 for file in files:
     data = read_info(file)
-# end = datetime.datetime.now() - start
-# print("Линейное:", end)
+# time_start = datetime.now()
+# for i in files:
+#    read_info(i)
+# time_end = datetime.now()
+# time_res = time_end - time_start
+# print(time_res)
 if __name__ == '__main__':
+    time_start = datetime.now()
     with multiprocessing.Pool(processes=4) as pool:
-        start_1 = datetime.datetime.now()
-        data_list = pool.map(read_info, files)
-        end_1 = datetime.datetime.now() - start_1
-        print("Многопроцессное:", end_1)
+        pool.map(read_info, files)
+    time_end = datetime.now()
+    time_res = time_end - time_start
+    print(time_res)
