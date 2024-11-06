8b30e76d557788e6ea0bf66f076707742b769eff
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-11-06 20:15:10 +0300

    Add files via upload

--- module_16_4.py
+++ module_16_4.py
@@ -1,52 +1,53 @@
-from http.client import HTTPException
-
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from pydantic import BaseModel
+from typing import Annotated
 
 app = FastAPI()
 
 users = []
 
+
 class User(BaseModel):
-    id: int = None
+    id: int
     username: str
-    age: int = None
-@app.get('/users')
-async def get_users() -> list:
+    age: int
+
+
+@app.get(path='/users')
+async def get_message() -> list[User]:
     return users
 
-@app.post('/user/{username}/{age}')
-async def add_user(user: User,
-                   username: str = Path(min_length=5, max_length=20, description='Enter username', example='Ivan'),
-                   age: int = Path(ge=18, le=120, description='Enter your age', example='16')) -> str:
-    user.id = 1 if len(users) == 0 else len(users) + 1
-    user.username = username
-    user.age = age
-    users.append(user)
-    return f'User {user.id} is registered.'
 
-@app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int,
-                      username: str = Path(min_length=5, max_length=20, description='Enter username',
-                                           example='Ivan'),
-                      age: int = Path(ge=18, le=120, description='Enter your age', example='34')) -> str:
-    try:
-        edit_user = next(user for user in users if user.id == user_id)
-        edit_user.username = username
-        edit_user.age = age
-        return f'User {user_id} has been updated.'
-    except Exception:
-        raise HTTPException(status_code=404, detail='User not found')
-@app.delete('/user/{user_id}')
-async def delete (user_id: int):
-    raise2 = True
-    ind_del = 0
-    for delete_user in users:
-        if delete_user.id == user_id:
-            users.pop(ind_del)
-            return delete_user
-        ind_del += 1
-    if raise2:
+@app.post(path='/user/{username}/{age}')
+async def post_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                    age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+    new_id = (users[-1].id + 1) if users else 1
+    new_user = User(id=new_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
+
+
+@app.put(path='/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+
+    else:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete(path='/user/{user_id}')
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')]) -> User:
+    for i, user in enumerate(users):
+        if user.id == user_id:
+            return users.pop(i)
+
+    else:
         raise HTTPException(status_code=404, detail='User was not found')
 
 # uvicorn module_16_4:app
