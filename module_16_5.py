949a2c86e8ba4d9089dca74c1a0d14c5db9ee8cc
Vinchesterg1990 <vinchesterg1990@gmail.com>
2024-11-06 20:17:39 +0300

    Add files via upload

--- module_16_5.py
+++ module_16_5.py
@@ -1,63 +1,66 @@
 from fastapi import FastAPI, Path, HTTPException, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
 from pydantic import BaseModel
+from typing import Annotated
 
 app = FastAPI()
-tmpl = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory='templates')
+
 users = []
 
 
 class User(BaseModel):
-    id: int = None
-    username: str = None
-    age: int = None
+    id: int  # номер пользователя(int)
+    username: str  #имя пользователя(str)
+    age: int  #возраст пользователя (int)
 
 
 @app.get('/')
-async def get_page(request: Request):
-    return tmpl.TemplateResponse("users.html", {'request': request, 'users': users})
+async def get_all_message(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse('users.html', {'request': request, 'users': users})
 
 
-@app.get('/user/{user_id}')
-async def get_user(request: Request, user_id: int):
-    try:
-        user = next(user for user in users if user.id == user_id)
-        return tmpl.TemplateResponse("users.html", {'request': request, 'user': user})
-    except Exception:
-        raise HTTPException(status_code=404, detail='User not found')
+@app.get(path='/user/{user_id}')
+async def get_message(request: Request, user_id: int) -> HTMLResponse:
+
+    user = next((user for user in users if user.id == user_id), None)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    else:
+        return templates.TemplateResponse('users.html', {'request': request, 'user': user})
 
 
-@app.post('/user/{username}/{age}')
-async def add_user(user: User,
-                   username: str = Path(min_length=5, max_length=20, description='Enter username', example='NewUser'),
-                   age: int = Path(ge=18, le=120, description='Enter User age', example='19')) -> str:
-    user.id = 1 if len(users) == 0 else len(users) + 1
-    user.username = username
-    user.age = age
-    users.append(user)
-    return f'User {user.id} is registered.'
+@app.post(path='/user/{username}/{age}')
+async def post_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                    age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+    new_id = (users[-1].id + 1) if users else 1
+    new_user = User(id=new_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
 
 
-@app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int,
-                      username: str = Path(min_length=5, max_length=20, description='Enter username',
-                                           example='NewUser'),
-                      age: int = Path(ge=18, le=120, description='Enter User age', example='19')) -> str:
-    try:
-        edit_user = next(user for user in users if user.id == user_id)
-        edit_user.username = username
-        edit_user.age = age
-        return f'User {user_id} has been updated.'
-    except Exception:
-        raise HTTPException(status_code=404, detail='User not found')
+@app.put(path='/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+
+        else:
+            raise HTTPException(status_code=404, detail="User was not found")
 
 
-@app.delete('/user/{user_id}')
-async def delete(user_id: int) -> str:
-    try:
-        del_user = next(user for user in users if user.id == user_id)
-        users.remove(del_user)
-        return f'User {user_id} has been deleted.'
-    except Exception:
-        raise HTTPException(status_code=404, detail='User not found')
-# uvicorn module_16_5:app
+@app.delete(path='/user/{user_id}')
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')]) -> User:
+    for i, user in enumerate(users):
+        if user.id == user_id:
+            return users.pop(i)
+
+    else:
+        raise HTTPException(status_code=404, detail='User was not found')
